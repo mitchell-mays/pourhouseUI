62b1bfd44959d60f2e18a00c8042c038731311ba
swkane <samuelwkane@gmail.com>
2018-09-06 09:08:25 -0400

    basic login form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+
+const LoginForm = () => (
+  <Form>
+    <Form.Field>
+      <label>Username</label>
+      <input placeholder="Username" />
+    </Form.Field>
+    <Form.Field>
+      <label>Password</label>
+      <input placeholder="password" />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="I agree to the Terms and Conditions" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
+);
+
+export default LoginForm;
