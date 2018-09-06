ca2b02546a5f4441931110f4499d92a8d3a90b01
swkane <samuelwkane@gmail.com>
2018-09-06 09:11:38 -0400

    header added to login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import { Button, Checkbox, Form, Message } from "semantic-ui-react";
 
 const LoginForm = () => (
-  <Form>
-    <Form.Field>
-      <label>Username</label>
-      <input placeholder="Username" />
-    </Form.Field>
-    <Form.Field>
-      <label>Password</label>
-      <input placeholder="password" />
-    </Form.Field>
-    <Form.Field>
-      <Checkbox label="I agree to the Terms and Conditions" />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
+  <Message>
+    <Message.Header>Login</Message.Header>
+    <Form>
+      <Form.Field>
+        <label>Username</label>
+        <input placeholder="username" />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input placeholder="password" />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label="I agree to the Terms and Conditions" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
+  </Message>
 );
 
 export default LoginForm;
